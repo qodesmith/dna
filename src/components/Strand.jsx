1e9b2ea3322423b3f36682357f8145d185611053
Aaron Cordova <qodesmith@fb.com>
2019-07-15 06:46:17 -0700

    Circles now growing & shrinking to mimic perspective.

--- src/components/Strand.jsx
+++ src/components/Strand.jsx
@@ -5,17 +5,22 @@ const s = num => `${num}s`
 
 const Strand = ({ size = 20, delay = 0 }) => {
   const strandStyle = { width: px(size * 8) }
-  const circleStyle = { width: px(size), height: px(size), animationDelay: s(delay) }
-  const barStyle = { width: px(size * 6), height: px(size / 5), animationDelay: s(delay) }
+  const animDelay = { animationDelay: s(delay) }
+  const circleStyle = { width: px(size), height: px(size), ...animDelay }
+  const barStyle = { width: px(size * 6), height: px(size / 5), ...animDelay }
 
   return (
     <div className="df mha align-items-center mt1" style={strandStyle}>
-      <div className="circle one" style={circleStyle} />
+      <div className="circle-container one" style={animDelay}>
+        <div className="circle one" style={circleStyle} />
+      </div>
       <div className="relative">
         <div className="bar" style={barStyle} />
         <div className="bar two" style={barStyle} />
       </div>
-      <div className="circle two" style={circleStyle} />
+      <div className="circle-container two" style={animDelay}>
+        <div className="circle two" style={circleStyle} />
+      </div>
     </div>
   )
 }
